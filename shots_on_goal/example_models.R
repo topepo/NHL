769c9116beddc08279dcc844df12397dae20721b
Max Kuhn <mxkuhn@gmail.com>
2021-12-07 19:24:09 -0500

    a few more models

--- shots_on_goal/example_models.R
+++ shots_on_goal/example_models.R
@@ -41,7 +41,7 @@ ranger_tune <-
   tune_grid(
     ranger_workflow,
     resamples = val_split,
-    grid = 10,
+    grid = 30,
     control = ct
   )
 
@@ -58,13 +58,30 @@ rpart_tune <-
 
 # ------------------------------------------------------------------------------
 
+set.seed(498)
+fda_tune <-
+  discrim_flexible(prod_degree = tune()) %>%
+  tune_grid(ranger_recipe,
+            resamples = val_split,
+            grid = 2,
+            control = ct)
+# ------------------------------------------------------------------------------
+
+set.seed(498)
+nb_tune <-
+  naive_Bayes() %>%
+  fit_resamples(ranger_recipe,
+                resamples = val_split,
+                control = ct)
+# ------------------------------------------------------------------------------
+
 
 glmnet_recipe <-
   recipe(formula = on_goal ~ ., data = shots_on_goal) %>%
   step_lencode_mixed(shooter, outcome = vars(on_goal)) %>%
-  step_dummy(all_nominal(), -all_outcomes()) %>%
+  step_dummy(all_nominal_predictors()) %>%
   step_zv(all_predictors()) %>%
-  step_normalize(all_predictors(), -all_nominal())
+  step_normalize(all_numeric_predictors())
 
 glmnet_spec <-
   logistic_reg(penalty = tune(), mixture = tune()) %>%
@@ -92,6 +109,48 @@ glmnet_tune <-
 
 # ------------------------------------------------------------------------------
 
+
+gam_recipe <-
+  recipe(formula = on_goal ~ ., data = shots_on_goal) %>%
+  step_lencode_mixed(shooter, outcome = vars(on_goal)) %>%
+  step_zv(all_predictors())
+
+gam_spec <-
+  gen_additive_mod() %>%
+  set_mode("classification")
+
+gam_workflow <-
+  workflow() %>%
+  add_recipe(gam_recipe) %>%
+  add_model(gam_spec, formula = on_goal ~ s(running) + s(shot_distance) + s(shot_angle))
+
+gam_tune <-
+  fit_resamples(
+    gam_workflow,
+    resamples = val_split,
+    control = ct
+  )
+
+gam_fit <-
+  gam_workflow %>%
+  fit(data = analysis(val_split$splits[[1]]))
+
+gam_grid_recipe <-
+  recipe(formula = on_goal ~ ., data = shots_on_goal) %>%
+  step_profile(-on_goal, -shot_angle, profile = vars(shot_angle), grid = list(pctl = TRUE, len = 500)) %>%
+  prep() %>%
+  bake(new_data = NULL)
+
+predict(gam_fit, gam_grid_recipe, type = "prob") %>%
+  bind_cols(predict(gam_fit, gam_grid_recipe, type = "conf_int")) %>%
+  bind_cols(gam_grid_recipe) %>%
+  ggplot(aes(x = shot_angle, y = .pred_yes)) +
+  geom_path() +
+  geom_ribbon(aes(ymin = .pred_lower_yes, ymax = .pred_upper_yes), alpha = .1) +
+  ylim(0:1)
+
+# ------------------------------------------------------------------------------
+
 show_best(ranger_tune)
 show_best(rpart_tune)
 show_best(glmnet_tune)
@@ -107,11 +166,12 @@ NHL_red <- NHL_blue <- NHL_light_blue <- rgb(0, 0, 0, .2)
 
 rpart_pred <-
   rpart_tune %>%
-  collect_predictions(parameters = select_best(rpart_tune, metric = "rocc_auc")) %>%
+  collect_predictions(parameters = select_best(rpart_tune, metric = "roc_auc")) %>%
   mutate(
     prediction = ifelse(.pred_class == on_goal, "correct", "error"),
     prediction = factor(prediction, levels = c("error", "correct"))
     ) %>%
+  select(-on_goal) %>%
   inner_join(shots_on_goal %>% add_rowindex(), by = ".row")
 
 set.seed(1)
@@ -137,7 +197,6 @@ nhl_rink_plot() +
     panel.border = element_blank()
   )
 
-
 # ------------------------------------------------------------------------------
 
 get_curve_data <- function(x, label) {
@@ -150,6 +209,9 @@ get_curve_data <- function(x, label) {
 bind_rows(
   get_curve_data(rpart_tune, "CART"),
   get_curve_data(glmnet_tune, "glmnet"),
+  get_curve_data(fda_tune, "FDA"),
+  get_curve_data(gam_tune, "GAM"),
+  get_curve_data(nb_tune, "naive Bayes"),
   get_curve_data(ranger_tune, "Random Forest"),
 ) %>%
   ggplot(aes(x = 1 - specificity, y = sensitivity, col = model)) +
